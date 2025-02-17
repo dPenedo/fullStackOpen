40c45722b1d317e5e65ee59b0bf34d048b1492a6
dpenedo <dpenedo@protonmail.com>
2025-02-17 14:05:28 -0300

    Part 1 finished

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,30 +1,75 @@
 import { useState } from "react";
 
-const Button = ({ text }) => {
-  return <button>{text}</button>;
+const Button = ({ text, voteToHanddle, setVotes }) => {
+  const handleClick = () => {
+    setVotes((prevVotes) => ({
+      ...prevVotes,
+      [voteToHanddle]: prevVotes[voteToHanddle] + 1,
+    }));
+  };
+  return <button onClick={handleClick}>{text}</button>;
 };
 
 const Counter = ({ text, number }) => {
   return (
-    <p>
-      <span>{text}</span> <span>{number}</span>
-    </p>
+    <tr>
+      <td>{text}</td>
+      <td>{number}</td>
+    </tr>
+  );
+};
+
+const StatisticLine = ({ text, value }) => {
+  return (
+    <tr>
+      <td> {text}</td>
+      <td>{value} </td>
+    </tr>
+  );
+};
+
+const Statistics = ({ good, neutral, bad }) => {
+  const allVotes = good + neutral + bad;
+  const averageVotes = (good - bad) / allVotes;
+  const positivePercent = (good / allVotes) * 100;
+  if (allVotes) {
+    return (
+      <>
+        <StatisticLine text={"All"} value={allVotes} />
+        <StatisticLine text={"Average"} value={averageVotes} />
+        <StatisticLine text={"Positive"} value={positivePercent} />
+      </>
+    );
+  }
+  return (
+    <tr>
+      <td>No feedback Given</td>
+    </tr>
   );
 };
 
 const App = () => {
-  const [count, setCount] = useState([0, 0, 0]);
+  const [votes, setVotes] = useState({ good: 0, neutral: 0, bad: 0 });
 
   return (
     <>
       <h2>Give fiiedback</h2>
-      <Button text="Good" />
-      <Button text="Neutral" />
-      <Button text="Bad" />
-      <h2>Give statistic</h2>
-      <Counter text="Good" number="0" />
-      <Counter text="Neutral" number="0" />
-      <Counter text="Bad" number="0" />
+      <Button setVotes={setVotes} voteToHanddle="good" text="Good" />
+      <Button setVotes={setVotes} voteToHanddle="neutral" text="Neutral" />
+      <Button setVotes={setVotes} voteToHanddle="bad" text="Bad" />
+      <h2>Statistic</h2>
+      <table>
+        <tbody>
+          <Counter text="Good" number={votes.good} />
+          <Counter text="Neutral" number={votes.neutral} />
+          <Counter text="Bad" number={votes.bad} />
+          <Statistics
+            good={votes.good}
+            neutral={votes.neutral}
+            bad={votes.bad}
+          />
+        </tbody>
+      </table>
     </>
   );
 };
